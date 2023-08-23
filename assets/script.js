016c1f7c21c9f86c66a4499547a8a674e1b01c34
Loïc Dupin <loic.dupin1@gmail.com>
2023-08-23 08:54:59 +0200

    Ajout d'une classe pour le Dot_Selected

--- assets/script.js
+++ assets/script.js
@@ -16,7 +16,62 @@ const slides = [
     tagLine: "Autocollants avec découpe laser sur mesure",
   },
 ];
-// let imgSlider = document.getElementsByClassName("imgSlider");
+
+let dot_slider = document.getElementsByClassName("dot_slider");
+let suivant = document.querySelector(".suivant");
+let precedent = document.querySelector(".precedent");
+let numero = 0;
+let nombreImages = slides.length;
+let nombreDots = dot_slider.length;
+function enleverActiveImages() {
+  for (let i = 0; i < nombreDots; i++) {
+    dot_slider[i].classList.remove("dot_selected");
+  }
+}
+const bannerPara = document.getElementById("texte");
+
+console.log(slides[numero]["image"]);
+function ChangeSlides(sens) {
+  numero = numero + sens;
+  enleverActiveImages();
+  if (numero >= nombreImages) {
+    numero = 0;
+  } else if (numero < 0) {
+    numero = nombreImages - 1;
+  }
+  document.getElementById("slide").src =
+    "./assets/images/slideshow/" + slides[numero]["image"];
+  // console.log(numero);
+  const bannerPara = document.getElementById("texte");
+  bannerPara.innerText = slides[numero]["tagLine"];
+  console.log(slides[numero]["image"]);
+}
+let dot_active = document.getElementsByClassName("dot");
+function ChangeDots(sens) {
+  dot_active[numero].classList.add("dot_selected");
+}
+
+suivant.addEventListener("click", plus);
+function plus() {
+  ChangeSlides(+1);
+  ChangeDots(+1);
+}
+precedent.addEventListener("click", moins);
+function moins() {
+  ChangeSlides(-1);
+  ChangeDots(-1);
+}
+setInterval("ChangeSlides(1)", 3000);
+setInterval("ChangeDots(1)", 3000);
+// suivant.addEventListener("click", function () {
+//   numero = numero + sens;
+//   if (numero >= nombreImages) {
+//     numero = 0;
+//   } else if (numero < 0) {
+//     numero = nombreImages - 1;
+//   }
+//   dot_active[numero].classList.add("dot_selected");
+// });// let imgSlider = document.getElementsByClassName("imgSlider");
 // console.log(imgSlider);
 // console.log(nombreImages);
 // // let precedent = document.querySelector(".precedent");
@@ -50,58 +105,3 @@ const slides = [
 // flecheDroite.addEventListener("click", () =>
 //   console.log("Click sur la flèche de droite !")
 // );
-let dot_slider = document.getElementsByClassName("dot_slider");
-console.log(dot_slider);
-let suivant = document.querySelector(".suivant");
-let precedent = document.querySelector(".precedent");
-let numero = 0;
-let nombreImages = slides.length;
-let nombreDots = dot_slider.length;
-function enleverActiveImages() {
-  for (let i = 0; i < nombreDots; i++) {
-    dot_slider[i].classList.remove("dot_selected");
-  }
-}
-
-const bannerPara = document.getElementById("texte");
-
-console.log(slides[numero]["image"]);
-function ChangeSlides(sens) {
-  numero = numero + sens;
-  enleverActiveImages();
-  if (numero >= nombreImages) {
-    numero = 0;
-  } else if (numero < 0) {
-    numero = nombreImages - 1;
-  }
-  document.getElementById("slide").src =
-    "./assets/images/slideshow/" + slides[numero]["image"];
-  // console.log(numero);
-  const bannerPara = document.getElementById("texte");
-  bannerPara.innerText = slides[numero]["tagLine"];
-}
-let dot_active = document.getElementsByClassName("dot");
-function ChangeDots(sens) {
-  dot_active[numero].classList.add("dot_selected");
-}
-// suivant.addEventListener("click", function () {
-//   numero = numero + sens;
-//   if (numero >= nombreImages) {
-//     numero = 0;
-//   } else if (numero < 0) {
-//     numero = nombreImages - 1;
-//   }
-//   dot_active[numero].classList.add("dot_selected");
-// });
-suivant.addEventListener("click", plus);
-function plus() {
-  ChangeSlides(+1);
-  ChangeDots(+1);
-}
-precedent.addEventListener("click", moins);
-function moins() {
-  ChangeSlides(-1);
-  ChangeDots(-1);
-}
-setInterval("ChangeSlides(1)", 3000);
-setInterval("ChangeDots(1)", 3000);
